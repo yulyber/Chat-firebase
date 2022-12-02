e98d394b9bdb5b827b81ddd0a64af6648f30d504
yulyber <113151593+yulyber@users.noreply.github.com>
2022-12-01 22:16:54 -0500

    configuración firebase

---
+++ lib/config/configuration.dart
@@ -0,0 +1,13 @@
+ class Configuration {
+  static const apiKey = "AIzaSyBtzNDvZSAts_HnTV-SNbfdzdZrQGJgcKs";  // tomado de current_key
+  static const authDomain = "nrc2291-equipo02-chat-yb.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL =
+      "https://nrc2291-equipo02-chat-yb-default-rtdb.firebaseio.com/"; // tomado de firebase_url
+  static const projectId = "nrc2291-equipo02-chat-yb"; // tomado de project_id
+  static const storageBucket = "nrc2291-equipo02-chat-yb.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "1055219171079"; //no lo vamos a usar
+  static const appId = "1:1082403556990:web:86236fd624a6f2e86b922f"; // tomado de mobilesdk_app_id
+  static const measurementId = "G-QBD1Y95JR7"; //no lo vamos a usar
+
+
+}
